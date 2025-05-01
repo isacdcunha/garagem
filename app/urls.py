f303441a40a38ee293c9ffdee861ad9e411bcd85
isabelleIFC <isabellecunhaifc@gmail.com>
2025-05-01 12:36:15 -0300

    feat: Criação da API cor

--- app/urls.py
+++ app/urls.py
@@ -9,11 +9,14 @@ from rest_framework.routers import DefaultRouter
 
 
 from core.views import AcessorioViewSet
+from core.views import CorViewSet
 from core.views import UserViewSet
+
 
 router = DefaultRouter()
 
 router.register(r'acessorios', AcessorioViewSet)
+router.register(r'cores', CorViewSet)
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 
 urlpatterns = [
