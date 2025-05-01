299642c5ee2a8b696fe3ede814e7b288194ad74c
isabelleIFC <isabellecunhaifc@gmail.com>
2025-05-01 12:50:52 -0300

    feat: Criação da API modelo

--- app/urls.py
+++ app/urls.py
@@ -10,6 +10,7 @@ from rest_framework.routers import DefaultRouter
 
 from core.views import AcessorioViewSet
 from core.views import CorViewSet
+from core.views import ModeloViewSet
 from core.views import UserViewSet
 
 
@@ -17,6 +18,7 @@ router = DefaultRouter()
 
 router.register(r'acessorios', AcessorioViewSet)
 router.register(r'cores', CorViewSet)
+router.register(r'modelos', ModeloViewSet)
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 
 urlpatterns = [
